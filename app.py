641c06c47d9698f74c25f85dfc74195873fb7a2f
mbreidenbach <matt12900@live.com>
2019-01-16 23:51:01 -0500

    got posting to work!

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask
 app = Flask(__name__)
+word = "Hello World"
 
-@app.route('/Search')
+@app.route('/users',methods=["GET"])
 def String_Returner():
-    return "Hello World!"
+    with open('word.txt') as f:
+        return f.readline()
+
+@app.route('/users',methods=["POST"])
+def Update_Word():
+    with open('word.txt', "w+") as f:
+        f.write("bet")
+    return "200"
 
